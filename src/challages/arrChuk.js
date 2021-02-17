c3297ff013196d7bc1c6ad61348fad7795dbc657
Dipen parmar <dipensavji@gmail.com>
2021-02-17 09:50:29 +0530

    isAnagram fizzbuzz arrChuk maxUsedChar isPalindrome

--- src/challages/arrChuk.js
+++ src/challages/arrChuk.js
@@ -1,9 +1,29 @@
 console.clear();
 
-const arrChuk = (input) => {
-  console.log(input);
+const arrChuk = (arr, chunkSize) => {
+  if (!Array.isArray(arr)) return false; // should be array
+  if (arr.constructor !== [].constructor) return false; // should be array
+  if (!Number.isFinite(chunkSize)) return false; // should be number
+  if (chunkSize < 0) return false; // should be greater then zero
+
+  let output = [];
+  for (let i = 0; i <= arr.length - 1; i += chunkSize) {
+    const arrChunk = arr.slice(i, i + chunkSize);
+    output.push(arrChunk);
+  }
+
+  // let inx = 0;
+  // while (inx < arr.length) {
+  //   output.push(arr.slice(inx, inx + chunkSize));
+  //   inx += chunkSize;
+  // }
+
+  return output;
 };
 
-arrChuk('Carr');
+arrChuk([1, 2, 3, 4, 5, 6, 7, 8], 2);
+arrChuk([1, 2, 3, 4, 5, 6, 7, 8], 3);
+// arrChuk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 1);
+// arrChuk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 5);
 
 module.exports = arrChuk;
