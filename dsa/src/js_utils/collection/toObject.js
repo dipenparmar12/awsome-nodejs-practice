93bbe1f976f2339cb9cf736446409688f9366962
Dipen parmar <dipensavji@gmail.com>
2022-05-26 13:19:43 +0530

    feat(js-utils): Add framwork agnostik js-utils

---
+++ dsa/src/js_utils/collection/toObject.js
@@ -0,0 +1,34 @@
+/**
+ * Convert an array of objects to a single object (duplicate of keyBy)
+ * @param {*} arr
+ * @param {*} key
+ * @returns {Object} obj
+ * @src https://1loc.dev/array/convert-an-array-of-objects-to-a-single-object/
+ */
+function toObject(arr, key) {
+  return arr.reduce((a, b) => ({ ...a, [b[key]]: b }), {})
+}
+// // Or
+// const toObject = (arr, key) =>
+//   Object.fromEntries(arr.map((it) => [it[key], it]))
+
+export default toObject
+
+//
+/*
+
+toObject(
+  [
+      { id: '1', name: 'Alpha', gender: 'Male' },
+      { id: '2', name: 'Bravo', gender: 'Male' },
+      { id: '3', name: 'Charlie', gender: 'Female' },
+  ],
+  'id'
+)
+
+{
+  '1': { id: '1', name: 'Alpha', gender: 'Male' },
+  '2': { id: '2', name: 'Bravo', gender: 'Male' },
+  '3': { id: '3', name: 'Charlie', gender: 'Female' },
+}
+*/
---
+++ dsa/src/js_utils/faker/getRandomInt.js
@@ -0,0 +1,18 @@
+/**
+ *
+ * @param {*} min
+ * @param {*} max
+ * @returns Integer
+ * @desc Returns a random number between min (inclusive) and max (exclusive)
+ * @src https://stackoverflow.com/a/1527820/8592918
+ */
+function getRandomInt(min = 0, max = 1000) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+// getRandomInt(1, 10) //  1 to 10
+// getRandomInt(400, 500) //  400 to 500
+
+export default getRandomInt
