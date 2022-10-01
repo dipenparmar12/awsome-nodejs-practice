fe534ef59d6ab633a0f1290a340d35991d550135
Dipen parmar <dipensavji@gmail.com>
2022-10-01 11:25:07 +0530

    fix(rename)

---
+++ dsa/dsa_/leetcode/3_isIsomorphic.py
@@ -0,0 +1,40 @@
+"""
+Definition: https://leetcode.com/problems/isomorphic-strings/?envType=study-plan&id=level-1
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+ 
+--- Example 1:
+Input: s = "egg", t = "add"
+Output: true
+
+--- Example 2:
+Input: s = "foo", t = "bar"
+Output: false
+
+
+--- Example 3:
+Input: s = "paper", t = "title"
+Output: true
+
+"""
+
+import os
+os.system('clear')
+
+
+"""
+
+
+"""
+
+
+def isIsomorphic(nums):
+
+    pass
+
+
+isIsomorphic([6, 4, 7, 3, 11, 4, 1, 15])  #
+isIsomorphic([2, 1, -1])
+isIsomorphic([1, 2, 3])  #
---
+++ dsa/dsa_/leetcode/_.py
@@ -0,0 +1,31 @@
+"""
+Definition: [URL]
+[String]
+ 
+--- Example 1:
+
+--- Example 2:
+
+
+--- Example 3:
+
+"""
+
+import os
+os.system('clear')
+
+
+"""
+
+
+"""
+
+
+def funcName(input):
+    print(input)
+    pass
+
+
+funcName(123)  #
+funcName(123)  #
+funcName(123)  #
