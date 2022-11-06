20d8bda082ea9571f80411a2a8e1358696cdd6b1
Dipen parmar <dipensavji@gmail.com>
2022-11-06 20:47:02 +0530

    refactor

--- src/challages/isPalindrome.js
+++
@@ -1,7 +0,0 @@
-console.clear();
-
-const isPalindrome = (str) => {
-  return str.split('').reverse().join('') === str;
-};
-
-module.exports = isPalindrome;
--- src/search/binary_search.js
+++
@@ -1,6 +0,0 @@
-console.log('dipen');
-let a = 10
-let b = 20
-
-
-let c = a + b
