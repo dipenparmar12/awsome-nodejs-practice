9c071f59c79b2ac25a829fbf10f03b294b40e4a7
Dipen parmar <dipensavji@gmail.com>
2022-11-06 22:01:28 +0530

    feat(docker): Node express app init.

---
+++ node_docker_env/src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bootExpress from './boot-express.js';
+
+console.clear()
+console.warn('-------------', new Date())
+console.log('app.js::[1]')
+
+const port = process.env.PORT || 8000
+const app = express()
+await bootExpress(app)
+
+app.listen(port, err => {
+  if (err) {
+    console.log('app.js::[15] err', err)
+    return process.exit(1);
+  }
+  console.log(`Server is running on ${port}`);
+});
+
+export default app
